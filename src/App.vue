0cd5a0708c53b29e2f826f83c7473e61042e0168
onikka10 <fertovaveronika@gmail.com>
2024-12-03 20:18:30 +0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import MyHello from './components/MyHello.vue';
+</script>
+
+<template>
+<MyHello title="Кроссовки Nike"  :price="15000" is-added/>
+<MyHello  is-Added/>
+<MyHello :is-Added="false"/>
+<MyHello />
+<MyHello />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/MyHello.vue
@@ -0,0 +1,26 @@
+<script setup>
+   const props = defineProps({
+    title: String,
+    price: Number,
+    isAdded: Boolean
+   });
+   
+console.log(props);
+
+</script>
+
+<template>
+    <div class="product">
+        <h2>Товар: {{title}}</h2>
+        <h3>Цена: {{ price }}</h3>
+        <h3>Добавлен:{{ isAdded }}</h3>
+    </div>
+</template>
+
+<style scoped>
+.product {
+    border: 1px solid blue;
+    padding: 10px;
+    margin-bottom: 10px;
+}
+</style>
