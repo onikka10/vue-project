5fff76285ea7b2d570bd91a36a3127329dcdd6b1
onikka10 <fertovaveronika@gmail.com>
2024-12-03 19:57:15 +0500

    Add files via upload

---
+++ src/components/MyHello.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+    
+let count = ref(0)
+function increment() {
+    count.value++
+    console.log(count)
+    
+}
+function decrement() {
+    count.value--;
+    console.log(count)
+    
+}
+</script>
+
+<template>
+    <h1>{{ count }}</h1>
+    <button @click="increment" >+</button>
+    <button @click="decrement">-</button>
+
+</template>
