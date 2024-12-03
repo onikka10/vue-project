b3ddaea52a2661ff725976563245257f92428236
onikka10 <fertovaveronika@gmail.com>
2024-12-03 20:40:25 +0500

    Add files via upload

---
+++ src/components/MyHello.vue
@@ -0,0 +1,29 @@
+<script setup>
+   const props = defineProps({
+    title: String,
+    price: Number,
+    isAdded: Boolean
+   });
+
+console.log(props);
+
+</script>
+
+<template>
+    <div class="product">
+        <h2>Товар: {{title}}</h2>
+        <h3>Цена: {{ price }}</h3>
+        <h3>Добавлен:{{ isAdded }}</h3>
+        
+        <slot></slot>
+    </div>
+    
+</template>
+
+<style scoped>
+.product {
+    border: 1px solid blue;
+    padding: 10px;
+    margin-bottom: 10px;
+}
+</style>
