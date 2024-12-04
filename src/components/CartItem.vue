6d41b98ba49ba0d6434319055f60f97dc90d11b9
onikka10 <fertovaveronika@gmail.com>
2024-12-04 21:05:22 +0500

    Add files via upload

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -13,10 +13,10 @@ defineProps({
   <img class="w-16 h-16" src="/sneakers/sneakers-1.jpg" alt="Sneakers" />
 
   <div class="flex flex-col w-full">
-    <p>Мужские кросовки</p>
+    <p>Krosovka</p>
     
     <div class="flex justify-between mt-2">
-      <b>12990 руб</b>
+      <b>12990 rb</b>
       <img class="opacity-40 hover:opacity-100 cursor-pointer transition" src="/close.svg" alt="delete" />  
     </div>
   </div>
